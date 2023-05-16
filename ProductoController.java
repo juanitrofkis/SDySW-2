9e60ca4371163b52741c57855e9081312bb99217
juanitrofkis <30332038+juanitrofkis@users.noreply.github.com>
2023-05-16 15:13:36 +0200

    Add files via upload

--- ProductoController.java
+++ ProductoController.java
@@ -23,122 +23,107 @@ public class ProductoController {
     private String url = "jdbc:postgresql://localhost:5432/dit";
     private String user = "dit";
     private String pass = "dit";
+	try{	
+		Connection conn = DriverManager.getConnection(url, user, pass);
+		Statement st = conn.createStatement();
+	} catch (Exception e) {
+			System.err.println(e.getClass().getName()+": "+e.getMessage());
+			System.exit(0);
+	}
+		
 
     @PostMapping("/tienda/addProductoNew") 
     public ResponseEntity<String> addProductoNew(@RequestBody Producto producto) 
     {
-	//Class.forName("org.postgresql.Driver");
-	try{
-	    
-	    Connection conn = DriverManager.getConnection(url, user, pass);
-	    PreparedStatement st = conn.prepareStatement("INSERT INTO productos (nombre, precio, cantidad) VALUES (?,?,?)");
-	    st.setString(1, producto.getNombre());
-		st.setFloat(2, producto.getPrecio());
-		st.setInt(3, producto.getCantidad());
-
-	    int rowsInsert = st.executeUpdate();
+		//Obtenemos el ID más alto y le sumamos 1
+		int id=0;
 		
-		if(rowsInsert > 0){
-			ResultSet generarLlave = statement.getGenerateKeys();
-			if(generarLlave.next()){
-				int id = generarLlave.getInt(1);
-				generarLlave.close();
-				if( id == 0)
-					return ResponseEntity.badRequest().body("Este producto ya existe en la DB");
-				else 
-					return ResponseEntity.ok("El producto se ha agregado correctamente. ID: "+ id);
+		ResultSet rs = st.executeQuery("SELECT MAX(ID) AS ID FROM INVENTARIO;");
+		
+		
+		if (rs.next() && cantidad >= 1) {
+			id = rs.getInt("ID")+1;
+			ResultSet rs2 = st.executeQuery("SELECT NOMBRE FROM INVENTARIO;");
+			Boolean repetido = false;
+			while(rs2.next())
+				if (rs2.getString("NOMBRE").equals(producto.getNombre()))
+					repetido = true;
+			
+			if (repetido) {
+				return ResponseEntity.badRequest().body("Este producto ya existe en la base de datos");
 			}
-		}
-		return ResponseEntity.badRequest().body("No se puedo generar el prodcuto correctamente");
-	 
-	    st.close();
-	    conn.close();
-	} catch (SQLException e) {
-		System.out.println("Excepción SQL Exception: " + e.getMessage());
-		e.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
-		}
+			else
+				st.executeUpdate("INSERT INTO INVENTARIO (ID,NOMBRE,PRECIO,CANTIDAD)"
+								+ " VALUES("+id+",'"+?+"',"+?+","+?+" );");
+				st.setString(1, producto.getNombre());
+				st.setFloat(2, producto.getPrecio());
+				st.setInt(3, producto.getCantidad());
+		} else if (producto.getCantidad() < 1)
+			return ResponseEntity.badRequest().body("La cantidad tiene que ser mayor o igual a 1");
+		return ResponseEntity.ok("El producto se ha agregado correctamente. ID: "+ id);
+		rs2.close();
+		rs.close();
     }
 
 
     @PutMapping("/tienda/addProducto") 
     public ResponseEntity<String> addProducto(@RequestBody Producto producto) 
     {
-	//Class.forName("org.postgresql.Driver");
-	try{
-	    
-	    Connection conn = DriverManager.getConnection(url, user, pass);
-	    PreparedStatement st = conn.prepareStatement("UPDATE productos SET nombre = ?, precio = ?, cantidad = ? WHERE id = ?");
-	    st.setString(1, producto.getNombre());
-		st.setFloat(2, producto.getPrecio());
-		st.setInt(3, producto.getCantidad());
-        st.setInt(4, producto.getId());
-
-	    int rowsUpdate = st.executeUpdate();
 		
-		if(rowsInsert > 0){
-			return ResponseEntity.ok("El producto se ha agregado correctamente. ID: "+ producto.getId());
-		}
-		return ResponseEntity.badRequest().body("No se puedo introducir el prodcuto correctamente");
-	 
-	    st.close();
-	    conn.close();
-	} catch (SQLException e) {
-		System.out.println("Excepción SQL Exception: " + e.getMessage());
-		e.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
-		}
+		int cantidad = producto.getCantidad();
+		int id = producto.getId();
+		ResultSet rs = st.executeQuery("SELECT CANTIDAD FROM INVENTARIO WHERE ID="+id+";");
+		st.setInt(1, producto.getId());
+		if (rs.next()) {
+			st.executeUpdate("UPDATE INVENTARIO SET CANTIDAD="+(rs.getInt("CANTIDAD")+cantidad)+" WHERE ID="+id+";");
+			
+		}else
+			return ResponseEntity.badRequest().body("No se puedo introducir el prodcuto correctamente");
+		
+		return ResponseEntity.ok("El producto se ha agregado correctamente. ID: "+ producto.getId());
+		rs.close();	
+	
     }
     @DeleteMapping("/tienda/eliminarProducto") 
     public ResponseEntity<String> deleteProducto(@RequestBody Producto producto) 
     {
-	//Class.forName("org.postgresql.Driver");
-	try{
-	    
-	    Connection conn = DriverManager.getConnection(url, user, pass);
-	    PreparedStatement st = conn.prepareStatement("DELETE FROM productos WHERE id = ?");
-        st.setInt(1, producto.getId());
-
-	    int rowsDeleted = st.executeUpdate();
-		
-		if(rowsDeleted > 0){
+	    int id = producto.getId();
+	    ResultSet rs = st.executeQuery("SELECT * FROM INVENTARIO WHERE ID ="+id);
+		if (!rs.next())
 			return ResponseEntity.ok("El producto se ha elimando correctamente.");
-		}
-		return ResponseEntity.badRequest().body("No se puedo eliminar el prodcuto correctamente");
-	 
-	    st.close();
-	    conn.close();
-	} catch (SQLException e) {
-		System.out.println("Excepción SQL Exception: " + e.getMessage());
-		e.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
-		}
+		else
+			st.executeUpdate("DELETE FROM INVENTARIO WHERE ID="+id+";");
+		rs.close();
+		
     }
     @GetMapping("/tienda/CashFlow") 
     public ResponseEntity<String> cashFlow() 
     {
-	//Class.forName("org.postgresql.Driver");
-	try{
-	    
-	    Connection conn = DriverManager.getConnection(url, user, pass);
-	    PreparedStatement st = conn.prepareStatement("SELECT SUM(cashFlow) AS flujo_caja FROM productos");
 
-	    ResultSet rs = st.executeQuery();
+			ResultSet rs = st.executeQuery("SELECT TOTAL FROM CUENTAS WHERE ID=0");
+			
+			while(rs.next())
+			{
+				float total = rs.getFloat("flujo_caja");
+				return ResponseEntity.ok("El flujo de caja de la tienda es: "+total);
+			}
+			rs.close();
+			return ResponseEntity.badRequest().body("No se ha podido obtener el flujo de caja");
+		 
 		
-		while(rs.next())
-		{
-		    float flujoDeCaja = rs.getFloat("flujo_caja");
-			return ResponseEntity.ok("El flujo de caja de la tienda es: "+flujoDeCaja);
-		}
-	    rs.close();
-		return ResponseEntity.badRequest().body("No se ha podido obtener el flujo de caja");
-	 
-	    st.close();
-	    conn.close();
-	} catch (SQLException e) {
-		System.out.println("Excepción SQL Exception: " + e.getMessage());
-		e.printStackTrace();
-		return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
-		}
     }
+	//cerramos la conexión a la base de datos
+	protected void finalize() throws Throwable{
+		try {
+			st.close();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
+		}
+		try {
+			conn.close();
+		} catch (Exception e) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error en el servidor");
+		}
+	}
+	
 }
